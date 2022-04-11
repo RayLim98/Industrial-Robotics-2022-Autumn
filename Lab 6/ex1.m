f0c742a7111d2ea285c3bb1527bffa0bd51801f8
Raymond Lim <rlim8868@gmail.com>
2022-04-12 09:26:56 +1000

    update

--- Lab 6/ex1.m
+++ Lab 6/ex1.m
@@ -1,6 +1,36 @@
 clc
 clf
 clear
-% s = Lab6Starter();
-% s.Test1
-mdl_puma
+
+workspace = [-2 2 -2 2 0 3];
+robot = SchunkUTSv2_0();
+q = [0,pi/2,0,0,0,0];
+robot.plot3d(q, 'workspace', workspace);
+view(3);
+camlight;
+hold on;
+
+tr = robot.fkine(q)
+startP = tr(1:3,4);
+endP = tr(1:3,4) + 1.9594 * tr(1:3,3);
+line1_h = plot3([startP(1),endP(1)],[startP(2),endP(2)],[startP(3),endP(3)],'r');
+plot3(endP(1),endP(2),endP(3),'r*');
+
+q = [ pi/10, pi/2, 0, 0, 0, 0 ];
+
+
+tr = robot.fkine(q)
+startP = tr(1:3,4);
+endP = tr(1:3,4) + 2.4861 * tr(1:3,3);
+line2_h = plot3([startP(1),endP(1)],[startP(2),endP(2)],[startP(3),endP(3)],'r');
+plot3(endP(1),endP(2),endP(3),'r*');
+
+q = [ -pi/10, 5*pi/12, 0, 0, 0, 0 ];
+tr = robot.fkine(q)
+startP = tr(1:3,4);
+endP = tr(1:3,4) + 1.9132 * tr(1:3,3);
+line3_h = plot3([startP(1),endP(1)],[startP(2),endP(2)],[startP(3),endP(3)],'r');
+plot3(endP(1),endP(2),endP(3),'r*');
+
+w1 = CreateYWall(1,2,[0,2]);
+
